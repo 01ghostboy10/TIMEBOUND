64490cf23dca662ad35e0e192765ddfec777d166
01ghostboy10 <levana.rai.simcik@mensa.cz>
2025-12-11 11:59:58 +0100

    I forgot to commit for a while lolol

--- timebound/☾ SCRIPTS/content_data.gd
+++ timebound/☾ SCRIPTS/content_data.gd
@@ -1,1 +1,17 @@
 extends Node
+
+@export var dialogPath = "resEDITHTISHIDSH"
+var phraseNum : int = 0
+var fininshed : bool = false
+var content_dict : Dictionary = {}
+var current_key = 0
+var current_value = 0
+
+
+
+func _ready():
+	load_content_dict()
+	
+func load_content_dict():
+	var f = FileAccess.open(dialogPath, FileAccess.READ)
+	assert(f.file_exists(dialogPath),)
