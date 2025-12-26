70b3828a72ba1e67c4e2ad29ceb8b69b74412f12
01ghostboy10 <levana.rai.simcik@mensa.cz>
2025-12-26 13:31:19 +0100

    archivists bg

---
+++ timebound/☾ SCENES/main.gd
@@ -0,0 +1,39 @@
+extends Control
+
+func _ready():
+	get_node("PanelContainer/MarginContainer/VBoxContainer/ScrollContainer/ContentContainer/Label").text = str("Prague, the Distant Future")
+	get_node("PanelContainer/MarginContainer/VBoxContainer/ScrollContainer/ContentContainer/content").text = str("""
+//The walls look suspicious. The glass ceiling looks suspicious. Everything looks like it's going to shove chloroform in your face any second now, especially when most people here are going to hate you by the end of the day.
+There evidently isn’t any reason to be anxious at all. Such a meeting with a past acquaintance will not harm them at all, only bruise the memories already scarred. And yet the buttons of his trenchcoat almost thrum, home to a thrashing heartbeat. The metro is unusually busy today, Eke mumbles with the objective of gunning his thoughts back to the present; he's greeted most Thursday mornings at this station but this one in particular is rowdier than he can recall.
+Near the A-line corridor, five –or is it six? his eyes glossed over their greasy heads nervously– rowdy teenagers wait to board the first screeching metal Frankenstein that times the opening of its doors perfectly, their faces reflecting in the window right next to where Eke can see his.
+
+Like the rest of Prague, the air is thick with the smell of petrol cars, petrichor and the unmistakeable distrust of all those around, though Eke is used to it of course. He bites into poppy-seed koláč (the grainy texture reminds him of the unintelligible announcements escaping from underfunded railway megaphones) and wipes his mouth with the back of his sleeve. Though poppies' petals usually resemble bloodshed when grown on a field, the centre seeds grow dark where rebirth has singed its centre black, blackened for consumption. A perfect metaphor for the city turned mould.
+Eke follows a few others toward the open doors of the metro, where disgruntled passengers mumble about delays caused by crumbling tunnels and rigidly uncaring board members. He picks at leftover seeds under his nails, flicking the debris onto the coats of tight-knit sepia and overcaffeinated passengers. The creaking wooden bench behind him tuts in disapproval as a man with blue-overalls and his equally azure companion get up.
+
+Minutes pass, but they feel like hours. Movement through space is fast, the subway systems make sure of that, and yet movement through time feels like dragging a ragdoll through knee-high swampland, sinking deeper and deeper into the ticking of the clock with each passing moment. Wish it differently, you might, but Eke knows the inevitable is not an easy thing to escape.
+The metal wheels screech and finally the rush and blur outside misty breathed-on windows slows to a stop, but not gracefully. People on people lurch forward, domino-style, as the metro car dry-heaves toward the polluted darkness awaiting ahead. Eke's stomach regrets eating the koláč.
+
+A pair of red mechanical doors slide open, vomiting out people who do not wish to stay there any longer. Eke is among many thrown out, feeling bile rising in his throat as he weakly distracts himself with the metronomic tapping of his shoes. Taking a breath in would undoubtedly make him better in any other predicament, but any other metro stop would circulate air cleaner than this. While his lungs yearn for air colder than the hearts of corrupt presidents, dust upon traffic effluvium with a dash of poorly bathed blue-collar workers.
+The world is spinning the wrong way. To Eke, a tunnel isn't the hole the metro lines descends into but the vision his crazed capilaries are pumping against, trying to make out what could be hiding behind dark patches materialising in his line of sight. He bites his tongue in the hope of drowning any feelings of malaise. How would he present himself? After all, those who act unbothered are the ones who get things done.
+Blood gushes to his fingertips, the redness pushing against his nails like faces against a packed tram.
+Just a moment, just any silence would have been nice, he predicts. A poster sloppily glued on a pillar reads "Nepříjemnosti? Dejte nám vědět!"
+"Any inconveniences? Let us know!"
+
+What lies. Eke almost laughs through the nausea.
+The world is not a kind place.
+
+Noiseless hammering into eardrums, people buzzing with news but with nowhere to share it, repetitive profanities echoing past escalators–
+"Zdovolením, mohli byste ukázat lístek?"
+"Excuse me, could you show your ticket?"
+
+A man stands in front of him, neon-black checkers displaying a startling collection od unearned confidence. Eke stiffens, the sickness leaving his body while being replaced with a much more sinister, hot counterpart resembling the beating anxiety that has possesed his just recently. His palms sweat.
+Right, tickets. Dammit.
+Eke's attempt at patting himself down to find a nonexistent ticket is feeble, of course. He pulls out many things that definitely aren't a ticket, like batteries, soldering wire, and a phone that looks extremely dated. You'd think tickets wouldn't apply to you when you saved an entire nation from collapse. Or at least, Eke will do soon.
+Trying to explain the absurdity of the situation, he fumbles with his Nokia 211, and hastily bends down to retrieve it.
+
+
+Something feels off.
+
+A gunshot cuts through the noise.A gunshot cuts through the noise.
+
+	""")
